c87bb674fc152288d6f75fa6bb866a1e8d273a1b
JeanFab0307 <jfrandriatsimivony@gamil.com>
2024-04-07 16:52:46 +0300

    Task 1: Embed espressions and functions in react

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import logo from './holberton-logo.jpg';
+import {getFullYear, getFooterCopy} from './utils';
+
+function App() {
+  const year = getFullYear();
+  const footer = getFooterCopy(false);
+  return (
+    <>
+      <div className='App-header'>
+        <img src={logo} alt="Holberton logo"/>
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className='App-body'>
+        <p>Login to acceess the full dashboard</p>
+      </div>
+
+      <div className='App-footer'>
+        <p>Copyright {year} - {footer}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
