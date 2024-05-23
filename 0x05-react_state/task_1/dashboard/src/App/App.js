71aef7c7b57f66bf1ede4ab172c8c75cf1e35f4f
JeanFab0307 <jfrandriatsimivony@gamil.com>
2024-05-23 09:39:57 +0300

    Task 1: Create controlled component

---
+++ 0x05-react_state/task_1/dashboard/src/App/App.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import Proptypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import BodySection from '../BodySection/BodySection';
+import { getLatestNotification } from '../utils/utils';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLogOut = this.handleLogOut.bind(this);
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+    this.state = {displayDrawer: false};
+  }
+
+  handleLogOut(event) {
+    if ((event.ctrlKey || event.metaKey) && event.key === 'h') {
+      alert('Logging you out');
+      this.logOut;
+    }
+  }
+
+  handleDisplayDrawer() {
+    this.setState({displayDrawer: true});
+  }
+
+  handleHideDrawer() {
+    this.setState({displayDrawer: false});
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleLogOut);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleLogOut);
+  }
+
+  render() {
+    const listCourses = [
+      {id: 1, name: 'ES6', credit: 60},
+      {id: 2, name: 'Webpack', credit: 20},
+      {id: 3, name: 'React', credit: 40}
+    ];
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: getLatestNotification() }
+    ];
+    const {isLoggedIn, logOut} = this.props;
+    return (
+      <>
+      <div className={css(styles.App)}>
+        <div className="HeaderSection">
+          <Notifications
+          listNotifications={listNotifications}
+          displayDrawer={this.state.displayDrawer}
+          handleDisplayDrawer={this.handleDisplayDrawer}
+          handleHideDrawer={this.handleHideDrawer}/>
+          <Header />
+        </div>
+        <div className={css(styles.appBody)}>
+          {isLoggedIn ?
+          (
+            <BodySectionWithMarginBottom title='CourseList'>
+              <CourseList listCourses={listCourses} />
+            </BodySectionWithMarginBottom>
+          ) :
+          (
+            <BodySectionWithMarginBottom title='Log in to continue'>
+              <Login />
+            </BodySectionWithMarginBottom>
+          )}
+          <BodySection title='News from the School'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Perspiciatis at tempora odio, necessitatibus repudiandae
+							reiciendis cum nemo sed asperiores ut molestiae eaque aliquam illo
+							ipsa iste vero dolor voluptates.
+            </p>
+          </BodySection>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+}
+}
+
+App.proptypes = {
+  isLoggedIn: Proptypes.bool,
+  logOut: Proptypes.func
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {return;}
+};
+
+// Styles
+const styles = StyleSheet.create({
+  App: {
+    position: "relative",
+    height: "100vh",
+    maxwidth: "100vw",
+    fontFamily: "Segoe UI, Tahoma, Geneva, Verdana, sans-serif",
+    boxSizing: "content-box",
+    margin: 0,
+  },
+  appBody: {
+    margin: "10px 25px",
+    textAlign: "justify"
+  }
+});
+
+export default App;
