2dde588dd2b270b1f6a17551b40a3e2e90e80620
JeanFab0307 <jfrandriatsimivony@gmail.com>
2024-06-02 21:45:15 +0300

    task 1 and 2: Add map and nestes elements

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,4 +1,4 @@
-const { fromJS } = require('immutable');
+import { fromJS } from "./node_modules/immutable/dist/immutable";
 
 export default function getImmutableObject (object) {
   return fromJS(object);
