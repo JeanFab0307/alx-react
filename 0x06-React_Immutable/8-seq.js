e3f646964c65b9805dcdb954e2555b34268fc437
JeanFab0307 <jfrandriatsimivony@gmail.com>
2024-06-03 07:44:10 +0300

    Correct task 8 again

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -3,10 +3,11 @@ import { Seq } from "./node_modules/immutable/dist/immutable";
 export default function printBestStudents(object) {
   const bestStudents = Seq(object)
     .filter(student => student.score > 70)
-    .map(student => {
-      student.firstName = capitalize(student.firstName);
-      student.lastName = capitalize(student.lastName);
-    });
+    .map(student => ({
+      ...student,
+      lastName : capitalize(student.lastName),
+      firstName : capitalize(student.firstName),
+    }));
 
   console.log(bestStudents.toObject());
 }
