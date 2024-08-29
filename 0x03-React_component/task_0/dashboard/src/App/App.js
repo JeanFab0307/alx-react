69d14634ef0517e286296ea5488fbc0c001ced38
JeanFab0307 <jfrandriatsimivony@gmail.com>
2024-08-29 21:21:59 +0300

    Update App and its tests

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -12,18 +12,21 @@ class App extends React.Component {
   constructor(props) {
     super(props);
   }
+
+  listCourses = [
+    {id: 1, name: 'ES6', credit: 60},
+    {id: 2, name: 'Webpack', credit: 20},
+    {id: 3, name: 'React', credit: 40}
+  ];
+
+  listNotifications = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'urgent', html: getLatestNotification() }
+  ];
+  
   render() {
-    const listCourses = [
-      {id: 1, name: 'ES6', credit: 60},
-      {id: 2, name: 'Webpack', credit: 20},
-      {id: 3, name: 'React', credit: 40}
-    ];
-    const listNotifications = [
-      { id: 1, type: 'default', value: 'New course available' },
-      { id: 2, type: 'urgent', value: 'New resume available' },
-      { id: 3, type: 'urgent', html: getLatestNotification() }
-    ];
-    const {isLoggedIn} = this.props;
+    const { isLoggedIn } = this.props;
     return (
       <>
       <div className="root-notifications">
