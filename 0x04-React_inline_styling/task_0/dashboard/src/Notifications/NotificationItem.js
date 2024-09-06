79973c5b26c7a98457ec620f1866022d1cd7c371
JeanFab0307 <jfrandriatsimivony@gmail.com>
2024-09-06 15:52:37 +0300

    inline styling

---
+++ 0x04-React_inline_styling/task_0/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class NotificationItem extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { type, value, html, markAsRead, id } = this.props;
+      return (
+        <>
+        {html ? <li onClick={() => markAsRead(id)} dangerouslySetInnerHTML={{ __html: html }} 
+        data-priority={type}>{value}</li> : 
+        <li onClick={() => markAsRead(id)} data-priority={type}>{value}</li>}
+        </>
+      );
+  }
+}
+
+NotificationItem.propTypes = {
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  __html: PropTypes.shape({
+		html: PropTypes.string
+	}),
+  markAsRead: PropTypes.func
+};
+
+NotificationItem.defaultProps = {
+  type: 'default'
+};
+
+export default NotificationItem;
