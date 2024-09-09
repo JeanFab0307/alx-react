a54a26974e0b83e25a25cc6dc0c68d22e96416f8
JeanFab0307 <jfrandriatsimivony@gmail.com>
2024-09-09 15:43:14 +0300

    Adding a local state for notifications

---
+++ 0x05-react_state/task_0/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+class NotificationItem extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { type, value, html, markAsRead, id } = this.props;
+      return (
+        <span className={css(styles.mobile)}>
+        {html ? <li className={type === 'default' ? css(styles.default) : css(styles.urgent)}
+        onClick={() => markAsRead(id)} dangerouslySetInnerHTML={{ __html: html }} 
+        data-priority={type}></li> : 
+        <li className={type === 'default' ? css(styles.default) : css(styles.urgent)}
+        onClick={() => markAsRead(id)} data-priority={type}>{value}</li>}
+        </span>
+      );
+  }
+}
+
+NotificationItem.propTypes = {
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  __html: PropTypes.shape({
+		html: PropTypes.string
+	}),
+  markAsRead: PropTypes.func
+};
+
+NotificationItem.defaultProps = {
+  type: 'default'
+};
+
+const screenSize = {
+  smallScreen: '@media only screen and (max-width: 900px)'
+}
+
+const styles = StyleSheet.create({
+  urgent: {
+    color: "red"
+  },
+  default: {
+    color: "blue"
+  },
+  mobile: {
+    [screenSize.smallScreen]: {
+      position: "relative",
+      display:  "block",
+      borderBottom: "1px solid black",
+      fontSize: "20px",
+      padding: "10px 0px 10px 8px",
+      listStyle: "none",
+      margin: "0px",
+      width: "100%"
+    }
+  }
+});
+
+export default NotificationItem;
