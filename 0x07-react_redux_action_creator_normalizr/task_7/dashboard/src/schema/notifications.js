c7b407ddeb1436186477c7caed6f2d571206eb8d
JeanFab0307 <jfrandriatsimivony@gmail.com>
2024-09-19 22:29:41 +0300

    Async Action Creators

---
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/schema/notifications.js
@@ -0,0 +1,30 @@
+import { schema, normalize } from 'normalizer';
+import notificationsData from '../../dist/notifications.json';
+
+const user = new schema.Entity('users');
+const message = new schema.Entity('messages', {}, {
+  idAttribute: 'guid'
+});
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message
+})
+
+const normalized = normalize(notificationsData, notification)
+
+function getAllNotificationsByUser (userId) {
+  // const newData = notificationsData
+  //   .filter((data) => data.id === userId)
+  //   .map((data) => data.context);
+  // return newData;
+  const messages = normalized.entities.message;
+  const notifications = normalize.entities.notification;
+  const newData = [];
+
+  for (const id in notifications) {
+    if (notifications[id].author === userId) {
+      newData.push(messages[notifications[id].context]);
+    }
+  }
+  return newData;
+}
